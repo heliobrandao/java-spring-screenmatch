652b07a730c0ea0a545b1ae667249d56c3d0dbb1
HELIO SERRANO BRANDAO JUNIOR <heliob79@gmail.com>
2024-04-18 09:06:58 -0300

    02-03-persisting-relationships

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -22,9 +22,8 @@ public class Serie {
         private String atores;
         private String poster;
         private String sinopse;
-        @Transient
+        @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
         private List<Episodio> episodios = new ArrayList<>();
-
         public Serie() {}
         public Serie(DadosSerie dadosSerie){
             this.titulo = dadosSerie.titulo();
@@ -49,7 +48,8 @@ public class Serie {
     }
 
     public void setEpisodios(List<Episodio> episodios) {
-        this.episodios = episodios;
+            episodios.forEach(e -> e.setSerie(this));
+            this.episodios = episodios;
     }
 
     public String getTitulo() {
@@ -117,6 +117,7 @@ public class Serie {
                 ", atores='" + atores + '\'' +
                 ", poster='" + poster + '\'' +
                 ", sinopse'" + sinopse + '\'' +
+                ", episodios'" + episodios + '\'' +
                 '}';
     }
 
